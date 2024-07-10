fb4cd85b1eec37a334ec839db023e2bc64788f78
Donald592 <donaldaltamirano2007@gmail.com>
2024-07-10 16:03:02 -0400

    Add edits and codes

--- 00-intro-working/intro.js
+++ 00-intro-working/intro.js
@@ -8,12 +8,26 @@ const colorfield =document.getElementById("color-field")
 const FontSizeField =document.getElementById("font size")
 const clickbutton =document.getElementById("click button")
 const text =document.getElementById("text")
+const checkbox =document.getElementById("checkbox")
 const body =document.body;
 // let (or var" to declare variables)
+
 function buttonPress() {
     text.innerHTML = "Wow! You pressed the button!";
+
     
     body.style.backgroundColor= colorfield.value;
     text.style;fontSize= `${fontSizeField.value}px` ;//Note:the [`] key is above the [Tab] key
-    let fail = `${fontSizeField.value}px`; // Apostrophe will not work
+    let fail = '${fontSizeField.value}px'; // Apostrophe will not work
 }  
+
+function checkboxChange() {
+    let checked = checkbox.checked;
+    console.log(`The checkbox has this state: ${checked}`);
+    
+    if (checked) {
+        body.style.outlineStyle = "solid";
+    } else {
+        body.style.outlineStyle = "hidden";
+    }
+}
