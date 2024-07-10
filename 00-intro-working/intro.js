4b86ea290b27c8a2e1f686b077abb04beb501b17
Donald592 <donaldaltamirano2007@gmail.com>
2024-07-10 14:48:26 -0400

    Add java script and button function

---
+++ 00-intro-working/intro.js
@@ -0,0 +1,19 @@
+// This is the JavaScript code
+// GUIDELINES:
+//  1. Give every variable/const smallest scope
+//  2. Grab HTML elements by tag and store in const
+//  3. Put all other code inside functions
+
+const colorfield =document.getElementById("color-field")
+const FontSizeField =document.getElementById("font size")
+const clickbutton =document.getElementById("click button")
+const text =document.getElementById("text")
+const body =document.body;
+// let (or var" to declare variables)
+function buttonPress() {
+    text.innerHTML = "Wow! You pressed the button!";
+    
+    body.style.backgroundColor= colorfield.value;
+    text.style;fontSize= `${fontSizeField.value}px` ;//Note:the [`] key is above the [Tab] key
+    let fail = `${fontSizeField.value}px`; // Apostrophe will not work
+}  
